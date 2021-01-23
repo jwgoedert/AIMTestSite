949dcfea9a91f25645ea5fa59518502813c1659c
jwgoedert <jwgoedert@gmail.com>
2021-01-22 20:33:42 -0600

    Added bootstrap and JS array to html code

--- app.js
+++ app.js
@@ -4,4 +4,48 @@ var me = {
     favoriteNumber: '7'
 };
 
-console.log(me);
+var classMates = [{
+    firstName: "William",
+    lastName: "Watson",
+    favoriteNumber: 7,
+}, {
+    firstName: 'Deb',
+    LastName: 'Dogba',
+    favoriteNumber: 7,
+}, {
+    firstName: 'Jen',
+    lastName: 'Bogle',
+    favoriteNumber: 14
+}, {
+    firstName: 'Miranda',
+    lastName: 'McNiel',
+    favoriteNumber: 6
+}, {
+    firstName: 'Lingli',
+    lastName: 'Gan',
+    favoriteNumber: 9
+}, {
+    firstName: 'Nick',
+    lastName: 'Palandri',
+    favoriteNumber: 6
+}, {
+    firstName: 'James',
+    lastName: 'Goedert',
+    favoriteNumber: 7
+}
+
+]
+
+var ul = document.createElement('ul');
+
+document.getElementById('classList').appendChild(ul);
+
+for (var i = 0; i < classMates.length; i++){
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.innerHTML += classMates[i].firstName;
+}
+
+
+// console.log(me);
+// console.log(classMates);
